27ffb634defb37a414455585a45896b0216259d7
Aiden Redmond <adnrdmnd@gmail.com>
2022-10-13 20:18:42 -0400

    valid function checking rows and col

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -10,6 +10,7 @@ board = [
     [0,4,9,2,0,6,0,0,7]
 ]
 
+#this function just prints the board. it also adds lines for pretty formatting.
 def print_board(board):
     for row in range(len(board)):
         if row % 3 == 0 and row != 0:
@@ -24,8 +25,21 @@ def print_board(board):
             else:
                 print (str(board[row][col]) + " ", end="")
 
+#find the first empty position
 def find_empty(board):
     for row in range(len(board)):
         for col in range(len(board[0])):
             if board[row][col] == 0:
                 return (row, col)
+
+#this is implementing the basic rules of sudoku. checking the row, the col, and the sqaure
+def valid(board, number, position):
+    #check the col
+    for i in range(len(board)):
+        if board[i][position[1]] == number and pos[0] != i:
+            return False
+
+    #check the row
+    for i in range(len(board[0])):
+        if board[position[0]][i] == number and pos[1] != i:
+            return False;
